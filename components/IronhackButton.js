77b93020dfaeaff617ddb96c45a5a7d3f433db76
Rafael Guerra <rafa99gm@hotmail.com>
2024-04-30 09:54:26 +0100

    solved

--- components/IronhackButton.js
+++ components/IronhackButton.js
@@ -4,7 +4,18 @@ class IronhackButton extends React.Component {
   }
 
   // Render an a tag to Ironhack's website
-  render() {}
+  render() {
+    return React.createElement(
+      "a",
+      { href: "https://ironhack.com", target: "_blank"},
+      "Click here to go to Ironhack's website!"
+    );
+  }
 }
-
+console.log(React.Component);
 // Find all DOM containers with the class ironhack_button, and render Like buttons into them.
+document.querySelectorAll(".ironhack_button").forEach((domContainer) => {
+  console.log(domContainer);
+  const root = ReactDOM.createRoot(domContainer);
+  root.render(React.createElement(IronhackButton));
+});
